7061f9d5ac975270c68de3cf7d531d3240473ac6
pitomec <rybka.mykhailo.e@gmail.com>
2026-03-05 18:34:02 +0000

    added edge uninstall script

---
+++ Install/remove-edge.ps1
@@ -0,0 +1,57 @@
+function ForceRemoveEdge {
+    # Based on work from loadstring1 & ave9858
+    Write-Output "> Forcefully uninstalling Microsoft Edge..."
+
+    $regView = [Microsoft.Win32.RegistryView]::Registry32
+    $hklm = [Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $regView)
+    $hklm.CreateSubKey('SOFTWARE\Microsoft\EdgeUpdateDev').SetValue('AllowUninstall', '')
+
+    # Create stub (Creating this somehow allows uninstalling edge)
+    $edgeStub = "$env:SystemRoot\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
+    New-Item $edgeStub -ItemType Directory | Out-Null
+    New-Item "$edgeStub\MicrosoftEdge.exe" | Out-Null
+
+    # Remove edge
+    $uninstallRegKey = $hklm.OpenSubKey('SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Microsoft Edge')
+    if ($null -ne $uninstallRegKey) {
+        Write-Output "Running uninstaller..."
+        $uninstallString = $uninstallRegKey.GetValue('UninstallString') + ' --force-uninstall'
+        Start-Process cmd.exe "/c $uninstallString" -WindowStyle Hidden -Wait
+
+        Write-Output "Removing leftover files..."
+
+        $edgePaths = @(
+            "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk",
+            "$env:APPDATA\Microsoft\Internet Explorer\Quick Launch\Microsoft Edge.lnk",
+            "$env:APPDATA\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Microsoft Edge.lnk",
+            "$env:APPDATA\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Tombstones\Microsoft Edge.lnk",
+            "$env:PUBLIC\Desktop\Microsoft Edge.lnk",
+            "$env:USERPROFILE\Desktop\Microsoft Edge.lnk",
+            "$edgeStub"
+        )
+
+        foreach ($path in $edgePaths) {
+            if (Test-Path -Path $path) {
+                Remove-Item -Path $path -Force -Recurse -ErrorAction SilentlyContinue
+                Write-Host "  Removed $path" -ForegroundColor DarkGray
+            }
+        }
+
+        Write-Output "Cleaning up registry..."
+
+        # Remove ms edge from autostart
+        reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "MicrosoftEdgeAutoLaunch_A9F6DCE4ABADF4F51CF45CD7129E3C6C" /f *>$null
+        reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "Microsoft Edge Update" /f *>$null
+        reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run" /v "MicrosoftEdgeAutoLaunch_A9F6DCE4ABADF4F51CF45CD7129E3C6C" /f *>$null
+        reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run" /v "Microsoft Edge Update" /f *>$null
+
+        Write-Output "Microsoft Edge was uninstalled"
+    }
+    else {
+        Write-Output ""
+        Write-Host "Error: Unable to forcefully uninstall Microsoft Edge, uninstaller could not be found" -ForegroundColor Red
+    }
+    
+    Write-Output ""
+}
+ForceRemoveEdge
